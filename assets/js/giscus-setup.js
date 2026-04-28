39839eb07e71c01c9623252b00e69dc4a959d0c0
Qinggezuishua1 <596997099@qq.com>
2026-04-28 01:21:43 +0000

    Deploying to gh-pages from @ Qinggezuishua1/Qinggezuishua1.github.io@2ad2e6d2a7c4f3efa889fda775f7b87d1734a105 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Qinggezuishua1/Qinggezuihua1.github.io",
+    "data-repo": "Qinggezuishua1/Qinggezuishua1.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
